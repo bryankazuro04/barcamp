ed2199a11db011be85ae3c12739242caf2dbb201
Bryan <mhdalana211201@gmail.com>
2023-07-18 00:14:01 +0700

    delete double title navbar

--- src/function.js
+++ src/function.js
@@ -6,14 +6,8 @@ import "./styles/style.css";
 import "./styles/responsive_768px.css";
 import "./styles/responsive_576px.css";
 import "./scripts/functions/theme-mode.js";
-import brand from "./scripts/components/nav-brand.js";
+import "./scripts/components/nav-brand";
 import main from "./scripts/functions/main-app.js";
-
-const navbar = document.querySelector(".navbar");
-const navBrand = document.createElement("nav-brand");
-
-navBrand.brandNavbar = brand;
-navbar.insertBefore(navBrand, navbar.firstChild);
 
 const imgBanner = document.querySelector(".brand-banner");
 imgBanner.setAttribute("src", `${img}`);
